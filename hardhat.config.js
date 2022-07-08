9637f8962a4035c103820914191d3d7b3a31dc50
caohongbo <papa_100@163.com>
2022-07-08 17:11:21 +0800

    rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+// Import and configure dotenv
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +20,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  // 连接测试网络
+  networks: {
+    rinkeby: {
+      url: process.env.URL,
+      accounts: [process.env.ACCOUNT],
+    },
+  },
 };
